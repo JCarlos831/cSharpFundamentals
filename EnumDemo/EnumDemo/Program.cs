69aa9b6ec43361e15962497d034ae96e4e2170ee
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-05-26 13:14:50 -0400

    Enum Demo and Strings Demo code along

---
+++ EnumDemo/EnumDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EnumDemo
+{
+    class MainClass
+    {
+        public enum ShippingMethod
+        {
+            RegularAirMail = 1,
+            RegisteredAirMail = 2,
+            Express = 3
+        }
+        public static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+
+            Console.WriteLine(method.ToString);
+
+            var methodName = "Express";
+            var shippingMehtod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+        }
+    }
+}
