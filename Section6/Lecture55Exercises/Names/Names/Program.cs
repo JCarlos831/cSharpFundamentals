2e9319e43330b8dc52b4ceab96f4417def109ae7
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-06-04 14:46:03 -0400

    Section 8 videos watched

---
+++ Section6/Lecture55Exercises/Names/Names/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Names
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            List<string> names = new List<string>();
+            var name = "";
+            do
+            {
+                Console.WriteLine("Enter a name\n");
+                name = Console.ReadLine();
+                names.Add(name);
+            } while (name != "");
+
+            foreach (var i in names)
+            {
+                Console.Write(i);
+            }
+        }
+    }
+}
---
+++ Section8/Lecture68ProceduralProgramming/WhatsYourNameRefactored/WhatsYourNameRefactored/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WhatsYourNameRefactored
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            var name = Console.ReadLine();
+            var reversed = ReverseName(name);
+            Console.WriteLine("Reversed name: " + reversed);
+        }
+
+        public static string ReverseName(string name)
+        {
+            var array = new char[name.Length];
+            for (int i = name.Length; i > 0; i--)
+            {
+                array[name.Length - i] = name[i - 1];
+            }
+            return new string(array);
+        }
+    }
+}
