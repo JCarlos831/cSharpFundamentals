73247ea6bf35ff0f896172473121f259de61e9c5
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-06-11 12:14:25 -0400

    Section 10 Debugging Done. End of C Sharp Tutorial For Beginners.

---
+++ Section9/Lecture73DirectoryAndDirectoryInfoDemo/Lecture73DirectoryAndDirectoryInfoDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Lecture73DirectoryAndDirectoryInfoDemo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"/Users/JuanCMontoya/Projects/Udemy/csharp/c_sharp_basics_w_mosh/folder1");
+
+            var files = Directory.GetFiles(@"/Users/JuanCMontoya/Projects/Udemy/csharp/c_sharp_basics_w_mosh/Section8", "*.sln*", SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            var directories = Directory.GetDirectories(@"/Users/JuanCMontoya/Projects/Udemy/csharp/c_sharp_basics_w_mosh/Section8", "*.*", SearchOption.AllDirectories);
+            foreach (var directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+
+            Directory.Exists("...");
+
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+        }
+    }
+}
---
+++ Section9/Lecture74PathDemo/Lecture74PathDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Lecture74PathDemo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var path = @"/Users/JuanCMontoya/Projects/Udemy/csharp/c_sharp_basics_w_mosh/HelloWorld/HelloWorld.sln";
+
+            var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);
+
+            Console.WriteLine("Extension: " + Path.GetExtension(path));
+            Console.WriteLine("File Name: " + Path.GetFileName(path));
+            Console.WriteLine("File Name without extension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory Name: " + Path.GetDirectoryName(path));
+        }
+    }
+}
